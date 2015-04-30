c9e2da5780325c7b040731eb0e8914ea421710a5
james robert williams & Alec Arme <student@epicodus.com>
2015-04-30 14:53:14 -0700

    Add Space#mark_by spec and code. Passes spec.

--- lib/space.rb
+++ lib/space.rb
@@ -1,9 +1,15 @@
 class Space
-  attr_reader(:x_coord, :y_coord)
+  attr_reader(:x_coord, :y_coord, :state)
 
   def initialize(coords)
     @x_coord = coords.fetch(:x_coord)
     @y_coord = coords.fetch(:y_coord)
+    @state = coords.fetch(:state)
+  end
+
+  def mark_by(player)
+binding.pry
+    @state = player.mark()
 
   end
 
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -19,9 +19,19 @@ end
 describe(Space) do
   describe("#x_coordinate") do
     it("returns the space's x_coordinate") do
-      test_space = Space.new({ x_coord: 1, y_coord: 2 })
+      test_space = Space.new({ x_coord: 1, y_coord: 2, state: nil })
       expect(test_space.x_coord()).to(eq(1))
     end
+  end
+
+  describe("#mark_by") do
+    it('let a player change the state of a space') do
+      test_player = Player.new(mark: "X")
+      test_space = Space.new(x_coord: 1, y_coord: 2, state: nil)
+      test_space.mark_by(test_player)
+      expect(test_space.state()).to(eq("X"))
+    end
+
   end
 
 end
