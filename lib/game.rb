5782c9fab40440e4b729129a82fbae8aca1cdfa5
james robert williams & Alec Arme <student@epicodus.com>
2015-04-30 17:05:46 -0700

    Add Player#active parameter andspec for swapping active player. Passes spec.

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,6 @@ attr_reader(:turn_count, :game_over, :player_1, :player_2)
   def initialize
     @player_1 = Player.new(mark: 'X', active: true)
     @player_2 = Player.new(mark: 'O', active: false)
-binding.pry
     @current_board = Board.new()
     @game_over = false
     @turn_count = 1
@@ -22,8 +21,13 @@ binding.pry
   def next_turn
     @game_over = true if @turn_count < 9
     @turn_count += 1
-    @active = false
 
+    if @player_1.active()
+      @player_1 = Player.new(mark: "X", active: false)
+      @player_2 = Player.new(mark: "O", active: true)
+    else
+      @player_1 = Player.new(mark: "X", active: true)
+      @player_2 = Player.new(mark: "O", active: false)
+    end
   end
-
 end
